f0fe14acd0df61f79db64abe715d5c6b71206765
LeangHuy <120023658+LeangHuy@users.noreply.github.com>
2024-10-21 16:29:43 +0700

    modify back and cancel button

--- src/components/StepperControl.jsx
+++ src/components/StepperControl.jsx
@@ -8,14 +8,14 @@ const StepperControl = ({ handleClick, currentStep, steps }) => {
                 onClick={() => handleClick()}
                 className={`'bg-white text-slate-400 capitalize py-2 px-8 rounded-lg font-semibold cursor-pointer border-2 border-slate-300
         hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out ${currentStep === 1 ? "opacity-50 cursor-not-allowed" : ""}`}>
-                cancel
+                {currentStep === steps.length - 3 ? "cancel" : "back"}
             </button>
             {/* next button */}
             <button
                 onClick={() => handleClick("next")}
                 className='bg-green-500 text-white capitalize py-2 px-8 rounded-lg font-semibold cursor-pointer
         hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out'>
-                {currentStep === steps.length - 1 ? "Confirm" : "Next"}
+                {currentStep === steps.length ? "Confirm" : "Next"}
             </button>
         </div>
     )
